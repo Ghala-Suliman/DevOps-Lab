0b421a98d9f14d1862d5a05f62b8cd9610b5a640
ju25dy <ju25dy.ad@gmail.com>
2025-01-20 13:17:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// رسالة ترحيبية عند التسجيل
+document.addEventListener("DOMContentLoaded", function() {
+    const signUpBtn = document.querySelector('.btn');
+    signUpBtn.addEventListener('click', function() {
+        alert('Thanks for signing up! Enjoy sharing your recipes.');
+    });
+});
